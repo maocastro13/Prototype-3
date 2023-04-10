3d177241600955ef7c2a729d2be42685e7fa2a91
Mauricio Andrés Castro Morales <maocastro13@gmail.com>
2023-04-10 09:45:26 -0500

    Commit 1

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject obstaclePrefab;
+    private Vector3 spawnPos = new Vector3(28, 0, 0);
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+    private PlayerController playerControllerScript;
+    void Start()
+    {
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
+        playerControllerScript = FindObjectOfType<PlayerController>();
+    }
+
+    void SpawnObstacle()
+    {
+        if(playerControllerScript.gameOver == false)
+        {
+            Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        }
+    }
+}
